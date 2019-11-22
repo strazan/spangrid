d349de5570a5df1e5e99774500e7d2a83a9bc2b1
strazan <sigge@labor.cool>
2019-11-22 15:20:46 +0100

    docs folder

--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
-module.exports = function spangrid(grid, min, max, columns) {
+function spangrid(grid, min, max, columns) {
     if (min > max) throw new Error('Max must be higher then min');
     if (max > 100) throw new Error('Max must be lower then 100');
     
-    grid.style.display = 'grid'
+    grid.style.display = 'grid';
     grid.style.setProperty('grid-template-columns', 'repeat(' + 100 + ', 1fr)');
 
-    let items = [...grid.children];
-    let rows = getRows(items, columns)
+    let children = [...grid.children];
+    let rows = getRows(children, columns);
 
     rows.forEach(row => {
-        let sum = 0
-        let rowSpan = []
+        let sum = 0;
+        let rowSpan = [];
         while (sum !== 100) {
-            rowSpan = []
+            rowSpan = [];
             for (let i = 0; i < columns; i++) {
-                rowSpan.push(getRandomSpan(min, max))
+                rowSpan.push(getRandomSpan(min, max));
             }
-            sum = rowSpan.reduce((a,b) => a+b)
+            sum = rowSpan.reduce((a,b) => a+b);
         }
         row.forEach((item, i) => {
-            setSpan(rowSpan[i], item)
+            setSpan(rowSpan[i], item);
         } )
     })
 }
 
 function getRandomSpan(min, max) {
-    return max - Math.floor(Math.random() * (1 + max - min))
+    return max - Math.floor(Math.random() * (1 + max - min));
 }
 
 function getRows(items, columns) {
-    let row = []
+    let row = [];
     let rows = [];
     items.forEach((item, i) => {
         if (i % columns === 0 && i !== 0) {
-            rows.push(row)
-            row = []
+            rows.push(row);
+            row = [];
         }
-        row.push(item)
+        row.push(item);
     })
     if (row) {
-        rows.push(row)
+        rows.push(row);
     }
     return rows;
 }
 
 function setSpan(span, item) {
-    item.style.gridColumn = `span ${span}`
+    item.style.gridColumn = `span ${span}`;
+}
+
+module.exports = {
+    spangrid:spangrid
 }
