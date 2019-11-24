6bed86ce6c608a166bbb8c7ddeb6aaae8c43029a
strazan <sigge@labor.cool>
2019-11-24 19:30:35 +0100

    made docs

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+spangrid(document.getElementById('example-one'),20,40,3)
+spangrid(document.getElementById('example-two'),15,35,4)
+spangrid(document.getElementById('example-three'),15,35,5)
+// spangrid(document.getElementById('posts'),15,35,4)
+spangrid(document.getElementById('title'),5,15,8)
